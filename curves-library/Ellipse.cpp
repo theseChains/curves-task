acdc659d2fbae97a0058c4aba088e7e14549e206
theseChains <rasim0103@gmail.com>
2024-08-19 15:08:59 +0300

    Add initial Ellipse class implementation

---
+++ curves-library/Ellipse.cpp
@@ -0,0 +1,36 @@
+#include "Ellipse.h"
+
+#include <cmath>
+
+namespace cadex
+{
+Ellipse::Ellipse(double xRadius, double yRadius)
+    : m_xRadius{ xRadius }, m_yRadius{ yRadius }
+{
+}
+
+Point3D Ellipse::getPoint(double t) const
+{
+    return { m_xRadius * std::cos(t), m_yRadius * std::sin(t), 0.0 };
+}
+
+double Ellipse::getXRadius() const
+{
+    return m_xRadius;
+}
+
+double Ellipse::getYRadius() const
+{
+    return m_yRadius;
+}
+
+void Ellipse::setXRadius(double xRadius)
+{
+    m_xRadius = xRadius;
+}
+
+void Ellipse::setYRadius(double yRadius)
+{
+    m_yRadius = yRadius;
+}
+}
---
+++ curves-library/Ellipse.h
@@ -0,0 +1,27 @@
+#ifndef CADEX_CURVES_LIBRARY_CIRCLE_H_
+#define CADEX_CURVES_LIBRARY_CIRCLE_H_
+
+#include "Curve.h"
+
+namespace cadex
+{
+class Ellipse : public Curve
+{
+public:
+    Ellipse(double xRadius, double yRadius);
+
+    Point3D getPoint(double t) const override;
+
+    double getXRadius() const;
+    double getYRadius() const;
+
+    void setXRadius(double xRadius);
+    void setYRadius(double yRadius);
+
+private:
+    double m_xRadius;
+    double m_yRadius;
+};
+}
+
+#endif
