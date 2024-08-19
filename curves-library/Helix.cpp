544ac5eeb5b01f93e580fb72e336cf86e0064790
theseChains <rasim0103@gmail.com>
2024-08-19 15:19:19 +0300

    Add initial implementation of Helix class

---
+++ curves-library/Helix.cpp
@@ -0,0 +1,36 @@
+#include "Helix.h"
+
+#include <cmath>
+
+namespace cadex
+{
+Helix::Helix(double radius, double step)
+    : m_radius{ radius }, m_step{ step }
+{
+}
+
+Point3D Helix::getPoint(double t) const
+{
+    return { m_radius * std::cos(t), m_radius * std::sin(t), m_step * t };
+}
+
+double Helix::getRadius() const
+{
+    return m_radius;
+}
+
+double Helix::getStep() const
+{
+    return m_step;
+}
+
+void Helix::setRadius(double radius)
+{
+    m_radius = radius;
+}
+
+void Helix::setStep(double step)
+{
+    m_step = step;
+}
+}
---
+++ curves-library/Helix.h
@@ -0,0 +1,27 @@
+#ifndef CADEX_CURVES_LIBRARY_HELIX_H_
+#define CADEX_CURVES_LIBRARY_HELIX_H_
+
+#include "Curve.h"
+
+namespace cadex
+{
+class Helix : public Curve
+{
+public:
+    Helix(double radius, double step);
+
+    Point3D getPoint(double t) const override;
+
+    double getRadius() const;
+    double getStep() const;
+
+    void setRadius(double radius);
+    void setStep(double step);
+
+private:
+    double m_radius;
+    double m_step;
+};
+}
+
+#endif
