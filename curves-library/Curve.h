dd76dc1def1d2611191ab1311fda847028f48ede
theseChains <rasim0103@gmail.com>
2024-08-19 15:13:05 +0300

    Add initial implementation of Curve class

---
+++ curves-library/Curve.h
@@ -0,0 +1,15 @@
+#ifndef CADEX_CURVES_LIBRARY_CURVE_H_
+#define CADEX_CURVES_LIBRARY_CURVE_H_
+
+#include "Point3D.h"
+
+namespace cadex
+{
+class Curve
+{
+public:
+    virtual Point3D getPoint(double t) const = 0;
+};
+}
+
+#endif
